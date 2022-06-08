792911b2dda3fe098ff38cbca92201386012549b
zhanggangbin <1738238581@qq.com>
2022-06-08 16:58:59 +0800

    first commit

---
+++ pages/aboutMe/aboutMe.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<p>{{name}}</p>
+		<button @click="hello">点我</button>
+	</view>
+</template>
+
+<script lang="ts">
+	export default {
+		onLoad() {
+			console.log('aboutme page');
+		},
+		data() {
+			return {
+				name: '关于我'
+			};
+		},
+		methods: {
+			hello() {
+				console.log('say hello')
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/aboutMe/aboutMe.js
@@ -0,0 +1,25 @@
+"use strict";
+var common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  onLoad() {
+    console.log("aboutme page");
+  },
+  data() {
+    return {
+      name: "\u5173\u4E8E\u6211"
+    };
+  },
+  methods: {
+    hello() {
+      console.log("say hello");
+    }
+  }
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {
+    a: common_vendor.t($data.name),
+    b: common_vendor.o((...args) => $options.hello && $options.hello(...args))
+  };
+}
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/myProject/uni-app-test/pages/aboutMe/aboutMe.vue"]]);
+wx.createPage(MiniProgramPage);
